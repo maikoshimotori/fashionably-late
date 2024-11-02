ec09be5f9a671fb198f107d519a78d9348df60d7
maiko <3430maiko@gmail.com>
2024-11-02 21:38:31 +0900

    ファイルの追加

---
+++ src/database/migrations/2024_11_02_200727_create_additional_users_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdditionalUsersTable extends Migration // クラス名を変更
+{
+    public function up()
+    {
+        Schema::create('additional_users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 255)->nullable(false);
+            $table->string('email', 255)->unique()->nullable(false);
+            $table->string('password', 255)->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('additional_users');
+    }
+}
